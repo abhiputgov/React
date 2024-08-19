c57d382a7b0cad86333bfeb7bdba3b7f070ab59b
Abhishek Puthezhath Govind <abhishek.govind@sap.com>
2024-08-20 00:16:40 +0530

    final rescafolding

--- 03-project/src/App.jsx
+++ 03-project/src/App.jsx
@@ -1,14 +1,26 @@
+import reactImg from './assets/react-core-concepts.png';
+
+const aReactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+function fnGetRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+function Header() {
+  const reactDescription = aReactDescriptions[fnGetRandomInt(2)];
+  return (
+    <header>
+      <img src={reactImg} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {reactDescription} React concepts you will need for almost any app you
+        are going to build!
+      </p>
+    </header>
+  );
+}
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header />
       <main>
         <h2>Time to get started!</h2>
       </main>
